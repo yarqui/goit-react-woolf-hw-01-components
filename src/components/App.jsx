42afd7607d4c3fc73c25a85bd50588752f52046a
yarqui <y.pelykh@gmail.com>
2024-03-28 23:36:34 -0400

    add Profile and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import user from '../data/user.json';
+import Profile from './Profile/Profile';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Profile user={user} />;
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import s from './Profile.module.css';
+
+const Profile = ({ user }) => {
+  const {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  } = user;
+
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} width={100} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li className={s.listItem}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li className={s.listItem}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li className={s.listItem}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
